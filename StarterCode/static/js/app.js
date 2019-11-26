790b5658956b2d347af04c1e147c6fdf5c33b2a9
JC Ketterer <jcketterer18@gmail.com>
2019-11-25 18:12:21 -0700

    final

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,33 +1,111 @@
-// from data.js
+var $tbody      = document.querySelector("tbody");
+var $Input      = document.querySelector("#search");
+var $searchBtn1 = document.querySelector("#datetime");
+var $searchBtn2 = document.querySelector("#city");
+var $searchBtn3 = document.querySelector("#state");
+var $searchBtn4 = document.querySelector("#country");
+var $searchBtn5 = document.querySelector("#shape");
+var $searchBtn0 = document.querySelector("#reset");
+
+$searchBtn0.addEventListener("click", reset);
+$searchBtn1.addEventListener("click", datetime);
+$searchBtn2.addEventListener("click", city);
+$searchBtn3.addEventListener("click", state);
+$searchBtn4.addEventListener("click", country);
+$searchBtn5.addEventListener("click", shape);
+
 var tableData = data;
+renderTable();
 
-//YOUR CODE HERE
-tbody = d3.select("tbody")
-
-// using Object.entries to get key, value data inside of the table
-// and loop through them to add to the table in html
-function displayData(data){ 
-    tbody.text("")
-    data.forEach(function(sighting){
-    new_tr = tbody.append("tr")
-    Object.entries(sighting).forEach(function([key, value]){
-        new_td = new_tr.append("td").text(value)	
-    })
-})}
-
-displayData(tableData)
-
-//select the web user's input and the filter button
-var dateInputText = d3.select("#datetime")
-var button = d3.select("filter-btn")
-
-// filter data with date that the user inputs
-function clickSelectDate(){
-    d3.event.preventDefault();
-    console.log(dateInputText.property("value"));
-    var new_table = tableData.filter(sighting => sighting.datetime===dateInputText.property("value"))
-    displayData(new_table);
+function reset() {
+    $Input.value = "";
+    renderTable();
 }
 
-// event listener to handle change and click
-dateInputText.on("change", clickSelectDate)
+function renderTable() {
+    $tbody.innerHTML = "";
+    for (var i = 0; i < data.length; i++) {
+        var address = data[i];
+        var fields = Object.keys(address);
+        var $row = $tbody.insertRow(i);
+        for (var j = 0; j < fields.length; j++) {
+            var field = fields[j];
+            var $cell = $row.insertCell(j);
+            $cell.innerText = address[field];
+        }
+    }
+}
+
+function datetime() {
+    // Format the user's search by removing leading and trailing whitespace, lowercase the string
+    var filteritem = $Input.value.trim().toLowerCase();
+  
+    // Set filteredAddresses to an array of all addresses whose "datetime" matches the filter
+    data = data.filter(function(address) {
+      var addressState = address.datetime.toLowerCase();
+  
+      // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+      return addressState === filteritem;
+    });
+    renderTable();
+  }
+
+  function city() {
+    // Format the user's search by removing leading and trailing whitespace, lowercase the string
+    var filteritem = $Input.value.trim().toLowerCase();
+  
+    // Set filteredAddresses to an array of all addresses whose "city" matches the filter
+    data = data.filter(function(address) {
+      var addressState = address.city.toLowerCase();
+  
+      // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+      return addressState === filteritem;
+    });
+    renderTable();
+  }
+
+  function state() {
+    // Format the user's search by removing leading and trailing whitespace, lowercase the string
+    var filteritem = $Input.value.trim().toLowerCase();
+  
+    // Set filteredAddresses to an array of all addresses whose "city" matches the filter
+    data = data.filter(function(address) {
+      var addressState = address.state.toLowerCase();
+  
+      // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+      return addressState === filteritem;
+    });
+    renderTable();
+  }
+
+  function country() {
+    // Format the user's search by removing leading and trailing whitespace, lowercase the string
+    var filteritem = $Input.value.trim().toLowerCase();
+  
+    // Set filteredAddresses to an array of all addresses whose "city" matches the filter
+    data = data.filter(function(address) {
+      var addressState = address.country.toLowerCase();
+  
+      // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+      return addressState === filteritem;
+    });
+    renderTable();
+  }
+
+  function shape() {
+    // Format the user's search by removing leading and trailing whitespace, lowercase the string
+    var filteritem = $Input.value.trim().toLowerCase();
+  
+    // Set filteredAddresses to an array of all addresses whose "city" matches the filter
+    data = data.filter(function(address) {
+      var addressState = address.shape.toLowerCase();
+  
+      // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+      return addressState === filteritem;
+    });
+    renderTable();
+  }
+
+
+
+
